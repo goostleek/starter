4bd17583e74767a00f0995f41bdd287e953296a6
Marcin Kłopotek <marcin.klopotek@jcommerce.pl>
2024-02-26 21:48:31 +0100

    Add custom defaults

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+-- Disable relative line numbers
+opt.relativenumber = false
+-- Set default EOL to LF
+opt.fileformat = "unix"
+-- Set default terminal to bash
+opt.shell = "bash"
+-- Set shell flags when executing commands
+opt.shellcmdflag = "-c"
+-- Enable system clipboard integration
+opt.clipboard = "unnamedplus"
