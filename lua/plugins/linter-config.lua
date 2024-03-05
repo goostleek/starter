cce525c82c40cbda0acd8095b8880d7feb3b7459
Marcin Kłopotek <marcin.klopotek@jcommerce.pl>
2024-03-05 11:55:49 +0100

    fixup! linter-config: Setup actionlint for GH workflows

--- lua/plugins/linter-config.lua
+++ lua/plugins/linter-config.lua
@@ -8,10 +8,9 @@ return {
       actionlint = {
         -- Run actionlint only if current file lays under .github/workflows
         condition = function(_)
-          local current_file_dir = vim.fn.expand("%:p:h")
           local workflow_dir_pattern = table.concat({ "", ".github", "workflows" }, require("plenary.path").path.sep)
           -- Check if the current_file_dir ends with the workflow_dir_pattern
-          return string.match(current_file_dir, workflow_dir_pattern .. "$")
+          return string.match(vim.fn.expand("%:p:h"), workflow_dir_pattern .. "$")
         end,
       },
     },
