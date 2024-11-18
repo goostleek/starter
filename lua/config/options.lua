68271ebe1b77a24381efa623092612800a8135dc
Marcin Kłopotek <marcin.klopotek@jcommerce.pl>
2024-11-18 16:23:10 +0100

    Tweak custom options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,26 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set cursor style to blinking
+-- opt.guicursor = "n-v-c-sm:block,ci-ve:ver25,r-cr-o:hor20,i:block-blinkwait700-blinkoff400-blinkon250-Cursor/lCursor"
+-- Disable relative line numbers
+-- opt.relativenumber = false
+-- Set default EOL to LF
+vim.o.fileformat = "unix"
+-- Set default terminal to bash
+vim.o.shell = "bash.exe"
+-- Set shell flags when executing commands
+vim.o.shellcmdflag = "-c"
+-- Set shell to use backslashes (Windows only)
+vim.o.shellslash = true
+-- Set shell to not quote arguments
+vim.o.shellxquote = ""
+-- Tweak the whitespace characters visibility
+local visibleWhitespace = vim.opt.listchars
+visibleWhitespace:append({ tab = "» " })
+visibleWhitespace:append({ trail = "·" })
+visibleWhitespace:append({ multispace = "·" })
+visibleWhitespace:append({ extends = ">" })
+visibleWhitespace:append({ precedes = "<" })
+visibleWhitespace:append({ nbsp = "␣" })
